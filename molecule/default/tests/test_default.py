c675548cf07161146eb4f9c94efaf5943ea585e7
sinist3rr <darkfloki@protonmail.com>
2019-10-27 17:11:57 +0200

    added some tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -7,9 +8,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('pkg', [
+  'mongodb-org',
+  'elasticsearch-oss',
+  'graylog-server',
+  'nginx'
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('svc', [
+  'mongod',
+  'elasticsearch',
+  'graylog-server',
+  'nginx'
+])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
